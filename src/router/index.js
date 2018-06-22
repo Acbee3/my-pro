f9b157a7909d9ac621f60e80ac019b9751d4287f
2463254281@qq.com <2463254281@qq.com>
2018-06-22 19:18:41 +0800

    登陆页面完毕

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Login from '@/components/login'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [{
     path: '/',
     redirect: '/login'
@@ -16,3 +16,17 @@ export default new Router({
   }
   ]
 })
+
+// 路由导航守卫的语法  router对象.beforeEach((to, from, next) => {})
+router.beforeEach((to, from, next) => {
+  // 如果访问的是登录页面则直接放行
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = sessionStorage.getItem('token')
+  // 如果有 token 直接放行
+  if (tokenStr) return next()
+  // 否则跳转到登陆页
+  next('/login')
+})
+
+export default router
